17c480c6f52e3bfcaea63ec153d171d968d94c7e
Amirhossein Gharebaghi <mycaspian81@gmail.com>
2024-03-29 22:37:03 +0330

    set smooth behavior for learn more click

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const closeCookie = document.querySelector('.btn--close-cookie');
+const btnScrolTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +22,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +32,45 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// create adn insert
+// const header = document.querySelector('.header');
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.innerHTML =
+//   'we used cookied for improved functionality and analystics. <button class = "btn btn--close-cookie">Got it!</button>';
+// header.prepend(message);
+
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', () => message.remove());
+
+// message.style.width = '120$';
+// message.style.backgroundColor = '#37383d';
+// console.log(getComputedStyle(message).height);
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height) + 40 + 'px';
+// console.log(message.style.height);
+
+btnScrolTo.addEventListener('click', e => {
+  e.preventDefault();
+  // const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+  // console.log(s1coords);
+  // console.log(window.pageXOffset, window.pageYOffset);
+  // window.scrollTo(
+  //   s1coords.keft + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.keft + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// btnScrolTo.onclick = function () {
+//   alert('hello');
+// };
